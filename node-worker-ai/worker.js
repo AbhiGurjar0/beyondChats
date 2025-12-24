d9f0ecf107baa959d7cf6922426e2671679ddbb5
Abhi Gurjar <abhishekbainsla190@gmail.com>
2025-12-24 19:13:32 +0530

    worker  done

--- node-worker-ai/worker.js
+++ node-worker-ai/worker.js
@@ -24,37 +24,32 @@ async function startHeavyGeneration(jobId, article_id) {
 
     const links = await searchGoogle(title);
     console.log("Found links:", links);
-    // // 3️⃣ Scrape competitor content
-    // const links = [
-    //   //   "https://medium.com/@S3CloudHub./choosing-the-right-ai-chatbot-a-comprehensive-guide-47fd478fe0ce",
-    //   "https://beyondchats.com/blogs/choosing-the-right-ai-chatbot-a-guide/",
-    // ];
+
     const competitorContents = [];
     for (const link of links) {
       competitorContents.push(await scrapeArticle(link));
     }
-    // console.log("Scraped competitor contents.", competitorContents);
+   
 
-    // // 4️⃣ Generate updated article
+    //  Generate updated article
     const updatedContent = await generateUpdatedArticle(
       title,
       content,
       competitorContents
     );
     console.log("Generated updated content.", updatedContent);
-    // 5️⃣ Append references
+    //  Append references
     const finalContent = `
     ${updatedContent}
 
     ---
-
     ### References
     1. ${links[0]}
     2. ${links[1]}
     `;
     console.log("Final content ready.", finalContent);
 
-    // // 6️⃣ Save to Laravel
+    //  Save to Laravel
     let created = await axios.post(`${LARAVEL_API}/api/articles`, {
       title: `${title} (Updated)`,
       content: finalContent,
@@ -62,8 +57,6 @@ async function startHeavyGeneration(jobId, article_id) {
       parent_id: article_id,
     });
 
-    // TEMP simulation (replace with real logic later)
-    console.log("Generated article for ID in Worker:", created.data);
     //set job id Status to completed in Laravel
     await axios.put(`${LARAVEL_API}/api/generation-jobs/update-status`, {
       job_id: jobId,
